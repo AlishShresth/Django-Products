f9529d4f89c10f6b5aeb1cacdb5010d089aca4d1
Alish Shrestha <cresthaalish@gmail.com>
2025-05-10 21:01:11 +0545

    function based views

---
+++ api/serializers.py
@@ -0,0 +1,80 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from .models import Order, Product, OrderItem
+
+
+User = get_user_model()
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "description",
+            "price",
+            "stock",
+        )
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than 0.")
+        return value
+
+
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "full_name",
+            "email",
+        )
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source="product.name")
+    product_price = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        source="product.price",
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = (
+            "product_name",
+            "product_price",
+            "quantity",
+            "item_subtotal",
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model = Order
+        fields = (
+            "order_id",
+            "created_at",
+            "user",
+            "status",
+            "items",
+            "total_price",
+        )
+
+
+class ProductInfoSerializer(serializers.Serializer):
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
